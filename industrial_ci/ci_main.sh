a978fce5d89fe746727521968c86fe6dd04e6d7e
Benjamin Maidel <bnm@ipa.fhg.de>
2016-12-02 17:31:01 +0100

    disable strict host key checking for github.com

--- industrial_ci/ci_main.sh
+++ industrial_ci/ci_main.sh
@@ -88,9 +88,10 @@ if [[ "$ROS_DISTRO" == "kinetic" ]] && ! [ "$IN_DOCKER" ]; then
   if [ $retval -eq 0 ]; then HIT_ENDOFSCRIPT=true; success 0; else exit; fi  # Call  travis_time_end  run_travissh_docker
 fi
 
-ssh-add -l
-
 travis_time_start init_travis_environment
+#setup github.com ssh key
+mkdir -p ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
 # Define more env vars
 BUILDER=catkin
 ROSWS=wstool
